715b96b54c759203d1e0dd493ba662e34b41647d
aboobakkar <aboobakkar.mec@gmail.com>
2021-09-01 12:46:16 +0530

    tailwind package removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import './tailwind.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
@@ -9,17 +8,6 @@ import SearchBox from './components/SearchBox';
 const App = () => {
 	const [movies, setMovies] = useState([]);
 	const [searchValue, setSearchValue] = useState('');
-
-	// const getMovieRequest = async (searchValue) => {
-	// 	const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
-
-	// 	const response = await fetch(url);
-	// 	const responseJson = await response.json();
-
-	// 	if (responseJson.Search) {
-	// 		setMovies(responseJson.Search);
-	// 	}
-	// };
 
 	const getData= async () => {
 		const response = await fetch('CONTENTLISTINGPAGE-PAGE1.json');
@@ -29,10 +17,6 @@ const App = () => {
 			setMovies(data.page)
 		}
 	  }
-
-	// useEffect(() => {
-	// 	getMovieRequest(searchValue);
-	// }, [searchValue]);
 
 	useEffect(()=>{
 		getData()
